ec8da8b7157a6ea0f1e5ff185a7d51b564d7901d
Travis Hinton <t@travishinton.us>
2021-09-22 13:04:50 -0600

    finished 06_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    if (typeof firstNumber === 'number' && typeof secondNumber === 'number') {
+    if (firstNumber < secondNumber) {
+    for (let index = firstNumber; index <= secondNumber; index++) {
+        sum = sum + index;
+    }
 
+    } else {
+        for (let index = secondNumber; index <= firstNumber; index++) {
+            sum = sum + index;
+        }
+    }
+
+    if (sum < 0 ) {
+        return 'ERROR';
+    } else return sum;
+} else {
+    return 'ERROR';
+}
 };
 
 // Do not edit below this line

607df79ab2a54f1fe207ac20a8dde43323e8d291
Travis Hinton <t@travishinton.us>
2021-09-22 12:44:40 -0600

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,20 @@
-const removeFromArray = function(array, ...valuesToRemove) {
+// First try - passes first test
+/* const removeFromArray = function(array, value) {
     oneToRemove = array.indexOf(value)
     array.splice(oneToRemove, 1);
     return array;
-};
+}; */
+
+// Second try - moving to using rest arguments and arrays
+const removeFromArray = function(array, ...valuesToRemove) {
+    for (let i = 0; i < valuesToRemove.length; i++) {
+        if (array.includes(valuesToRemove[i])) {
+        valueToRemove = array.indexOf(valuesToRemove[i]);
+        array.splice(valueToRemove, 1);
+        } else continue;        
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
